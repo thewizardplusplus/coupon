1a9766e110680a6f355cda92a10049d2e92772e0
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-09-13 23:00:00 +0300

    Add the output_coupon() function

--- coupon/output.py
+++ coupon/output.py
@@ -8,4 +8,11 @@ def output_coupons(coupons):
         output_coupon(coupon, base_path)
 
 def output_coupon(coupon, base_path):
-    ...
+    with open(os.path.join(
+        base_path,
+        'coupon_{}.html'.format(coupon['id']),
+    ), mode='x', encoding='utf-8') as coupon_file:
+        coupon_file.write(format_coupon(coupon))
+
+def format_coupon(coupon):
+    return 'test\n'
