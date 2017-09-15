d6e961cc931170bbedafcf24bb00e8393d6cf3f1
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-09-15 23:00:00 +0300

    Add the cut template filter

--- coupon/output.py
+++ coupon/output.py
@@ -8,6 +8,7 @@ from . import logger
 from . import consts
 
 PARAGRAPH_PATTERN = re.compile(r'\r\n|\n\r|\n|\r')
+SENTENCE_PATTERN = re.compile(r'\.(?:\W|$)')
 
 def output_coupons(coupons):
     # sets the locale setting for the datetime.strptime() function
@@ -22,6 +23,7 @@ def output_coupons(coupons):
     with open(os.environ['COUPON_TEMPLATE'], encoding='utf-8') as template_file:
         environment = jinja2.Environment(autoescape=False)
         environment.filters['format_timestamp'] = format_timestamp
+        environment.filters['cut'] = cut
         environment.filters['to_paragraphs'] = to_paragraphs
 
         template = environment.from_string(template_file.read())
@@ -44,6 +46,27 @@ def format_coupon(coupon, template):
 
 def format_timestamp(timestamp, format_=consts.ADMITAD_TIMESTAMP_FORMAT):
     return timestamp.strftime(format_)
+
+def cut(text, cut_mark):
+    first_paragraph, *rest_text = PARAGRAPH_PATTERN.split(
+        text.strip(),
+        maxsplit=1,
+    )
+    first_sentence, *rest_paragraph = SENTENCE_PATTERN.split(
+        first_paragraph,
+        maxsplit=1,
+    )
+    text_parts = [
+        sentence
+        for sentence in [first_sentence, *rest_paragraph, *rest_text]
+        for sentence in (sentence.strip(),)
+        if len(sentence) != 0
+    ]
+    return '\n'.join(
+        text_parts[:1] + [cut_mark] + text_parts[1:] \
+            if len(text_parts) > 1 \
+            else text_parts
+    )
 
 def to_paragraphs(text):
     return '\n'.join(
