39f75391d8387ccf38e4752a2e34f9ab11381af6
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-09-16 23:00:00 +0300

    Add the script.Lexer class

---
+++ coupon/script/lexer.py
@@ -0,0 +1,42 @@
+import json
+
+import ply.lex
+
+from .. import logger
+
+class Lexer:
+    tokens = [
+        'IDENTIFIER',
+        'REGULAR_EXPRESSION',
+        'MATCH_OPERATOR',
+        'AND_OPERATOR',
+        'OR_OPERATOR',
+    ]
+    t_IDENTIFIER = r'\w+'
+    t_REGULAR_EXPRESSION = r'/(?:\\.|[^/])+/'
+    t_MATCH_OPERATOR = '=~'
+    t_AND_OPERATOR = '&&'
+    t_OR_OPERATOR = '\|\|'
+    t_ignore = ' \t\n\r'
+    literals = '.!()'
+    errors = []
+
+    def __init__(self):
+        self._lexer = ply.lex.lex(module=self)
+
+    def input(self, code):
+        self._lexer.input(code)
+
+    def token(self):
+        return self._lexer.token()
+
+    @ply.lex.TOKEN('\#.*')
+    def t_SINGLE_LINE_COMMENT(self, token):
+        pass
+
+    def t_error(self, token):
+        err = 'illegal character ' + json.dumps(token.value[0])
+        self.errors.append(err)
+        logger.get_logger().warning(err)
+
+        self._lexer.skip(1)
--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import setuptools
 if not (0x030500f0 <= sys.hexversion < 0x040000a0):
     raise Exception('requires Python >=3.5, <4.0')
 
-package_name, *_ = setuptools.find_packages()
+packages = setuptools.find_packages()
+package_name = packages[0]
 setuptools.setup(
     name=package_name,
     version='1.0.0',
@@ -12,11 +13,12 @@ setuptools.setup(
     author='thewizardplusplus',
     author_email='thewizardplusplus@yandex.ru',
     url='https://github.com/thewizardplusplus/' + package_name,
-    packages=[package_name],
+    packages=packages,
     install_requires=[
         'termcolor >=1.1.0, <2.0',
         'python-dotenv >=0.7.1, <1.0',
         'admitad >=1.1.1, <2.0',
+        'ply >=3.10, <4.0',
         'Jinja2 >=2.9.6, <3.0',
     ],
     python_requires='>=3.5, <4.0',
