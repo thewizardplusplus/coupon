6c6f90bb4d3a9484a56cb129af73ded4cd39eea8
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-09-17 23:00:00 +0300

    Logging: add a logging to the init_env() function

--- coupon/env.py
+++ coupon/env.py
@@ -1,7 +1,14 @@
 import dotenv
+import termcolor
+
+from . import logger
 
 def init_env():
-    dotenv.load_dotenv(dotenv.find_dotenv(
-        usecwd=True,
-        raise_error_if_not_found=True,
-    ))
+    env_path = dotenv.find_dotenv(usecwd=True, raise_error_if_not_found=True)
+    dotenv.load_dotenv(env_path)
+
+    logger.get_logger().info(
+        'load the %s config %s',
+        termcolor.colored('.env', 'magenta'),
+        termcolor.colored(env_path, 'green'),
+    )
