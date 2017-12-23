f3af49a9548c6abea0a858d0cb8d1008c027d8ee
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-12-23 23:00:00 +0300

    Coupon cleaner: analyse timestamps

--- tools/coupon_cleaner.bash
+++ tools/coupon_cleaner.bash
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 export $(grep -v '^#' .env | xargs)
+declare current_timestamp="$(date +%s)"
 for file in $(find "$COUPON_OUTPUT_PATH" -maxdepth 1 -type f -name *.html); do
   declare filename="$(basename "$file" .html)"
-  declare timestamp="$(echo "$filename" | cut -d_ -f2)"
   declare coupon_id="$(echo "$filename" | cut -d_ -f3)"
-  echo "filename: \"$filename\""
-  echo "timestamp: \"$timestamp\""
-  echo "coupon ID: \"$coupon_id\""
+  declare timestamp="$(echo "$filename" | cut -d_ -f2)"
+  declare parsed_timestamp="$(date --date "$timestamp" +%s)"
+  echo "current timestamp:$current_timestamp"
+  echo "parsed timestamp: $parsed_timestamp"
+  if ((parsed_timestamp - current_timestamp < COUPON_TIMESTAMP_GAP)); then
+    echo "remove file $file"
+  fi
 done
