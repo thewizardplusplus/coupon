e57a108fa9743ffcab006fd272b4dbc6bcb631d3
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-12-23 23:00:00 +0300

    Coupon cleaner: parse a coupon filename

--- tools/coupon_cleaner.bash
+++ tools/coupon_cleaner.bash
@@ -2,5 +2,10 @@
 
 export $(grep -v '^#' .env | xargs)
 for file in $(find "$COUPON_OUTPUT_PATH" -maxdepth 1 -type f -name *.html); do
-  echo "\"$file\""
+  declare filename="$(basename "$file" .html)"
+  declare timestamp="$(echo "$filename" | cut -d_ -f2)"
+  declare coupon_id="$(echo "$filename" | cut -d_ -f3)"
+  echo "filename: \"$filename\""
+  echo "timestamp: \"$timestamp\""
+  echo "coupon ID: \"$coupon_id\""
 done
