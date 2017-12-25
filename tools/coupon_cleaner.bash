2dd7072582a3cbcc49f3974181cc1b3cb9b18485
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-12-25 23:00:00 +0300

    Coupon cleaner: extend a logging

--- tools/coupon_cleaner.bash
+++ tools/coupon_cleaner.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function log() {
-  declare -r message="$1"
+  declare -r level="$1"
+  declare -r message="$2"
 
-  echo "$(date --rfc-3339=ns) [INFO] $message" >&2
+  echo "$(date --rfc-3339=ns) [$level] $message" >&2
 }
 
 export $(grep -v '^#' .env | xargs)
@@ -15,11 +16,14 @@ for file in $(find "$COUPON_OUTPUT_PATH" -maxdepth 1 -type f -name *.html); do
   declare parsed_timestamp
   parsed_timestamp="$(date --date "$timestamp" +%s 2>/dev/null)"
   if [[ $? != 0 ]]; then
+    declare coupon_id="$(echo "$filename" | cut -d_ -f2)"
+    log WARNING "coupon #$coupon_id has an incorrect timestamp"
+
     continue
   fi
 
   if ((parsed_timestamp - current_timestamp < COUPON_TIMESTAMP_GAP)); then
-    log "remove the coupon #$coupon_id"
+    log INFO "remove the coupon #$coupon_id"
     rm -f "$file"
   fi
 done
