d1c7cd4ed980dc05466e18fa9aaf9654f757df1c
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-13 23:00:00 +0300

    Coupon remover: parse a coupon ID

--- tools/coupon_remover.bash
+++ tools/coupon_remover.bash
@@ -3,5 +3,7 @@
 export $(grep -v '^#' .env | xargs)
 declare -r output_path="${COUPON_OUTPUT_PATH:-./coupons/}"
 declare -r database="${COUPON_DATABASE:-./coupon.db}"
-echo "output path: $output_path"
-echo "database: $database"
+for file in $(find "$output_path" -maxdepth 1 -type f -name *.html); do
+  declare -i coupon_id="$(basename "$file" .html | awk -F_ '{print $NF}')"
+  echo "coupon ID: $coupon_id"
+done
