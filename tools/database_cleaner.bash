ba018fd4d556a86ab59dd30acdbc2b7dfb85f1aa
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-13 23:00:00 +0300

    Coupon remover: extend a logging

--- tools/database_cleaner.bash
+++ tools/database_cleaner.bash
@@ -8,10 +8,15 @@ function log() {
 }
 
 export $(grep -v '^#' .env | xargs)
+log INFO "load the .env config $(realpath .env)"
+
 declare -r output_path="${COUPON_OUTPUT_PATH:-./coupons/}"
+declare -r coupons="$(find "$output_path" -maxdepth 1 -type f -name *.html)"
+log INFO "find $(echo -n "$coupons" | wc -l) coupons"
+
 declare -r database="${COUPON_DATABASE:-./coupon.db}"
-for file in $(find "$output_path" -maxdepth 1 -type f -name *.html); do
-  declare -i coupon_id="$(basename "$file" .html | awk -F_ '{print $NF}')"
+for coupon in $coupons; do
+  declare -i coupon_id="$(basename "$coupon" .html | awk -F_ '{print $NF}')"
   log INFO "remove the coupon #$coupon_id from the database"
   sqlite3 -bail "$database" "
     DELETE FROM coupons WHERE coupon_id = $coupon_id
@@ -22,5 +27,5 @@ for file in $(find "$output_path" -maxdepth 1 -type f -name *.html); do
   fi
 
   log INFO "remove the coupon #$coupon_id from files"
-  rm -f "$file"
+  rm -f "$coupon"
 done
