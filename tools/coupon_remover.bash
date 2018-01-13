e1a724c83b100f8e08d415e6cf84ab8ed8909c72
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-13 23:00:00 +0300

    Coupon remover: remove a coupon from the database

--- tools/coupon_remover.bash
+++ tools/coupon_remover.bash
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+function log() {
+  declare -r level="$1"
+  declare -r message="$2"
+
+  echo "$(date --rfc-3339=ns) [$level] $message" >&2
+}
+
 export $(grep -v '^#' .env | xargs)
 declare -r output_path="${COUPON_OUTPUT_PATH:-./coupons/}"
 declare -r database="${COUPON_DATABASE:-./coupon.db}"
 for file in $(find "$output_path" -maxdepth 1 -type f -name *.html); do
   declare -i coupon_id="$(basename "$file" .html | awk -F_ '{print $NF}')"
-  echo "coupon ID: $coupon_id"
+  log INFO "remove the coupon #$coupon_id from the database"
+  sqlite3 -bail "$database" "
+    DELETE FROM coupons WHERE coupon_id = $coupon_id
+  " 2>/dev/null
+  if [[ $? != 0 ]]; then
+    log WARNING "unable to remove the coupon #$coupon_id from the database"
+    continue
+  fi
 done
